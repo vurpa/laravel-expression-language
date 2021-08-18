c5709e3fcfb131246e7cf0052eab636cabb26122
John <js@thesc.se>
2021-08-18 17:10:44 +0200

    Add expression syntax rule

---
+++ src/Rules/ExpressionSyntax.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Vurpa\ExpressionLanguage\Rules;
+
+use Vurpa\ExpressionLanguage\Facades\ExpressionLanguage;
+use Illuminate\Contracts\Validation\Rule;
+use Symfony\Component\ExpressionLanguage\SyntaxError;
+
+class ExpressionSyntax implements Rule
+{
+    private $names;
+
+    public function __constructor(?array $names) {
+        $this->names = $names;
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        try {
+            ExpressionLanguage::lint($value, $this->names);
+        } catch (SyntaxError $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'Invalid syntax';
+    }
+}
---
+++ tests/Rules/ExpressionSyntaxTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Vurpa\ExpressionLanguage\Tests\Rules;
+
+use Symfony\Component\ExpressionLanguage\SyntaxError;
+use Vurpa\ExpressionLanguage\Facades\ExpressionLanguage;
+use Vurpa\ExpressionLanguage\Rules\ExpressionSyntax;
+use Vurpa\ExpressionLanguage\Tests\TestCase;
+
+class ExpressionSyntaxTest extends TestCase
+{
+    public function test_passes_with_valid_syntax()
+    {
+        $rule = new ExpressionSyntax();
+
+        $this->assertTrue($rule->passes('expression', '1 + 2'));
+        $this->assertTrue($rule->passes('expression', 'a + b'));
+    }
+
+    public function test_fails_with_invalid_syntax()
+    {
+        $rule = new ExpressionSyntax();
+
+        $this->assertFalse($rule->passes('expression', '1 +'));
+    }
+}
